728c540ae6c94f612b2935fcaeefe02ffd126ffe
Trần Nhật Tiến <trannhattien160802@gmail.com>
2025-04-07 02:58:09 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  server: {
+    proxy: {
+      "/api/proxy": {
+        target: "http://fptsharelaptop.somee.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/proxy/, "/api"),
+      },
+    },
+  },
 });
